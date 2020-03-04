ac6b4ac0e0b7da358cc50a9a268ee16f0cad9d59
Connyhong <38686869+Connyhong@users.noreply.github.com>
2020-03-04 17:24:28 +0800

    Delete redis_backup.sh

--- HarborHA-master/redis/keepalived_cnf/slave/scripts/redis_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-REDISCLI="/usr/local/bin/redis-cli"
-LOGFILE="/var/log/keepalived-redis-state.log"
-echo "[backup]" >> $LOGFILE
-date >> $LOGFILE
-echo "Being slave...." >> $LOGFILE 2>&1
-sleep 15 
-echo "Run SLAVEOF cmd ..." >> $LOGFILE
-$REDISCLI SLAVEOF 192.168.99.11 6379 >> $LOGFILE  2>&1
